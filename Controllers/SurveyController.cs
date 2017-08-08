87777fddfce965a0c68e97c9bff8e69c3fbddb95
Troy Center <troycenter1@gmail.com>
2017-08-07 17:36:56 -0700

    app built and form ready... next, pass data...

---
+++ Controllers/SurveyController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace dojosurvey.Controllers
+{
+    public class SurveyController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
